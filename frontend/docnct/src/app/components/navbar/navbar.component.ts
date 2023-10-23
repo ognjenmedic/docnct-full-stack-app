cb4de5895bcc87d91f01b18befdd55cc24e450b7
ognjenmedic <ognjen.medic@gmail.com>
2023-10-23 23:46:30 +0200

    added modal to navbar

--- frontend/docnct/src/app/components/navbar/navbar.component.ts
+++ frontend/docnct/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ToggleComponentsService } from 'src/app/services/toggle-components.service';
+import { Component, OnInit } from "@angular/core";
+import { ToggleComponentsService } from "src/app/services/toggle-components.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
-  constructor(public toggleComponent: ToggleComponentsService) {}
+  public showMenu: boolean;
+
+  constructor(public toggleComponent: ToggleComponentsService) {
+    this.showMenu = false;
+  }
 
   ngOnInit(): void {}
 
   logoClickNavbar() {
     this.toggleComponent.logoClickNavbar();
+  }
+
+  openModal() {
+    this.showMenu = true;
+  }
+
+  closeModal() {
+    this.showMenu = false;
   }
 }
