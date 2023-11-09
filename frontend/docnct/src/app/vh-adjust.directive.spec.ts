2d25b44d4794a5a4bfdd7c9492be35d6b5ef12a2
ognjenmedic <ognjen.medic@gmail.com>
2023-11-09 23:59:18 +0100

    updated comments

--- frontend/docnct/src/app/vh-adjust.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VhAdjustDirective } from './vh-adjust.directive';
-
-describe('VhAdjustDirective', () => {
-  it('should create an instance', () => {
-    const directive = new VhAdjustDirective();
-    expect(directive).toBeTruthy();
-  });
-});
