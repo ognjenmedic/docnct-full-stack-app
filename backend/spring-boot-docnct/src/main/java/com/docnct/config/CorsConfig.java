e6c1c7c6efbab8d32c40662562fe8b63b6a155c3
ognjenmedic <ognjen.medic@gmail.com>
2024-01-08 20:29:10 +0100

    updated docker compose

--- backend/spring-boot-docnct/src/main/java/com/docnct/config/CorsConfig.java
+++ backend/spring-boot-docnct/src/main/java/com/docnct/config/CorsConfig.java
@@ -1,33 +1,33 @@
-package com.docnct.config;
+    package com.docnct.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.context.annotation.Profile;
+    import org.springframework.web.servlet.config.annotation.CorsRegistry;
+    import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+    @Configuration
+    public class CorsConfig {
 
-    @Bean
-    @Profile("dev")
-    public WebMvcConfigurer devCorsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-            }
-        };
+        @Bean
+        @Profile("dev")
+        public WebMvcConfigurer devCorsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                }
+            };
+        }
+
+        @Bean
+        @Profile("prod")
+        public WebMvcConfigurer prodCorsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedOrigins("http://54.197.133.211:81");
+                }
+            };
+        }
     }
-
-    @Bean
-    @Profile("prod")
-    public WebMvcConfigurer prodCorsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://54.197.133.211:81");
-            }
-        };
-    }
-}
