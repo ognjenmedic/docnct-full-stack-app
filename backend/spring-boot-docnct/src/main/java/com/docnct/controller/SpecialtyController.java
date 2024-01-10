643e741519a924c6d75988450f111ac460c219d8
ognjenmedic <ognjen.medic@gmail.com>
2024-01-10 01:47:10 +0100

    updated controllers

--- backend/spring-boot-docnct/src/main/java/com/docnct/controller/SpecialtyController.java
+++ backend/spring-boot-docnct/src/main/java/com/docnct/controller/SpecialtyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/specialties")
+@RequestMapping("/api/specialties")
 @CrossOrigin(origins="https://docpal.app")
 
 public class SpecialtyController {
