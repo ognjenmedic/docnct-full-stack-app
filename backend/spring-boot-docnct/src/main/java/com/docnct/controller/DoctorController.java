2df3ccc8714ba17406f1007fc0d099f65ae5c854
ognjenmedic <ognjen.medic@gmail.com>
2024-01-10 01:43:40 +0100

    updated controllers

--- backend/spring-boot-docnct/src/main/java/com/docnct/controller/DoctorController.java
+++ backend/spring-boot-docnct/src/main/java/com/docnct/controller/DoctorController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/doctors")
+@RequestMapping("api/doctors")
 @CrossOrigin(origins="https://docpal.app")
 
 public class DoctorController {
